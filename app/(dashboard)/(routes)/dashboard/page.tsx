e129f00b17724ea965744e7f91aaf69dc9f920b5
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-21 14:47:23 -0300

    feat: Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const DashboardPage = () => {
   return (
-    <div>Dashboard Page (protected)</div>
+    <div>
+      Dashboard Page (protected)
+      <UserButton afterSignOutUrl='/'/>
+    </div>
   )
 }
 
