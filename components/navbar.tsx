6eac66409a30aa6b34e6b5447e86832e2f26d777
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2023-08-22 05:43:14 -0300

    feat: api limit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "@/components/mobile-sidebar";
 
+
+
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
